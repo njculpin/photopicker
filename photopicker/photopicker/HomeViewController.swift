7bdee25f58d23f633c7cfbcf33a5295e961c6166
Nick <njculpin@gmail.com>
2018-11-23 10:45:10 -0800

    color select

--- photopicker/photopicker/HomeViewController.swift
+++ photopicker/photopicker/HomeViewController.swift
@@ -9,11 +9,8 @@
 import UIKit
 import Photos
 
-class HomeViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class HomeViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
-    var colors = 4
-    let filterNames = CIFilter.filterNames(inCategories: nil)
-
     lazy var selectedImage: UIImageView = {
         let si = UIImageView()
         si.layer.masksToBounds = true
@@ -31,17 +28,6 @@ class HomeViewController: UIViewController, UINavigationControllerDelegate, UIIm
     }()
     
     var imagePicker = UIImagePickerController()
-    
-    var colorCollectionView : UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        layout.minimumLineSpacing = 0
-        layout.scrollDirection = .vertical
-        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        cv.decelerationRate = UIScrollView.DecelerationRate.fast
-        cv.showsVerticalScrollIndicator = false
-        cv.backgroundColor = .clear
-        return cv
-    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,15 +37,9 @@ class HomeViewController: UIViewController, UINavigationControllerDelegate, UIIm
     func setUpViews(){
         self.view.addSubview(selectedImage)
         self.view.addSubview(selectImageButton)
-//        self.view.addSubview(colorCollectionView)
         
         selectedImage.fillSuperview()
         selectImageButton.anchor(nil, left: self.view.safeAreaLayoutGuide.leftAnchor, bottom: self.view.safeAreaLayoutGuide.bottomAnchor, right: self.view.safeAreaLayoutGuide.rightAnchor, topConstant: 0, leftConstant: 22, bottomConstant: 22, rightConstant: 22, widthConstant: 0, heightConstant: 44)
-//        colorCollectionView.anchor(self.view.safeAreaLayoutGuide.topAnchor, left: self.view.safeAreaLayoutGuide.leftAnchor, bottom: self.view.safeAreaLayoutGuide.bottomAnchor, right: self.view.safeAreaLayoutGuide.rightAnchor, topConstant: 44, leftConstant: 44, bottomConstant: 44, rightConstant: 44, widthConstant: 0, heightConstant: 0)
-//
-//
-//        colorCollectionView.delegate = self
-//        colorCollectionView.dataSource = self
     }
     
     @objc func pickImage(){
@@ -91,23 +71,9 @@ class HomeViewController: UIViewController, UINavigationControllerDelegate, UIIm
         let inputCIImage = CIImage(image: input)!
         let filter = CIFilter(name: "CICrystallize")!
         filter.setValue(inputCIImage, forKey: kCIInputImageKey)
-        filter.setValue(55, forKey: kCIInputRadiusKey)
+        filter.setValue(400, forKey: kCIInputRadiusKey)
         let outputImage = filter.outputImage!
         return UIImage(ciImage: outputImage)
     }
-    
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return colors
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let cell = UICollectionViewCell()
-//        cell.backgroundColor = .red
-//        return cell
-//    }
-//
-//    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//        return CGSize(width: colorCollectionView.bounds.size.width, height: colorCollectionView.bounds.size.height)
-//    }
     
 }
